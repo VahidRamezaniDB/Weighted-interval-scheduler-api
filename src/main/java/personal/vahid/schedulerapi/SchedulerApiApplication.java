2a3aba4848a2cf0721f794ef8036f49f21b16ebc
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2023-02-16 01:48:06 +0330

    inital commit

---
+++ src/main/java/personal/vahid/schedulerapi/SchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package personal.vahid.schedulerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApiApplication.class, args);
+    }
+
+}
